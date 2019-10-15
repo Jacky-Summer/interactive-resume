3e541e19c5e3821b1c09adadcbbc38756330c8fe
Jacky-Summer <1106619871@qq.com>
2019-10-15 23:43:13 +0800

    add detail section

--- js/index.js
+++ js/index.js
@@ -233,13 +233,136 @@ let messageRender = (function(){
     }
 })();
 
-let cubeRender = (function(){
+/*CUBE*/
+let cubeRender = (function () {
+    let $cubeBox = $('.cubeBox'),
+        $cube = $('.cube'),
+        $cubeList = $cube.find('li');
+
+    //=>手指控住旋转
+    let start = function start(ev) {
+        //=>记录手指按在位置的起始坐标
+        let point = ev.changedTouches[0];
+        this.strX = point.clientX;
+        this.strY = point.clientY;
+        this.changeX = 0;
+        this.changeY = 0;
+    };
+    let move = function move(ev) {
+        //=>用最新手指的位置-起始的位置，记录X/Y轴的偏移
+        let point = ev.changedTouches[0];
+        this.changeX = point.clientX - this.strX;
+        this.changeY = point.clientY - this.strY;
+    };
+    let end = function end(ev) {
+        //=>获取CHANGE/ROTATE值
+        let {changeX, changeY, rotateX, rotateY} = this,
+            isMove = false;
+        //=>验证是否发生移动（判断滑动误差）
+        Math.abs(changeX) > 10 || Math.abs(changeY) > 10 ? isMove = true : null;
+        //=>只有发生移动再处理
+        if (isMove) {
+            //1.左右滑=>CHANGE-X=>ROTATE-Y (正比:CHANGE越大ROTATE越大)
+            //2.上下滑=>CHANGE-Y=>ROTATE-X (反比:CHANGE越大ROTATE越小)
+            //3.为了让每一次操作旋转角度小一点，我们可以把移动距离的1/3作为旋转的角度即可
+            rotateX = rotateX - changeY / 3;
+            rotateY = rotateY + changeX / 3;
+            //=>赋值给魔方盒子
+            $(this).css('transform', `scale(0.6) rotateX(${rotateX}deg) rotateY(${rotateY}deg)`);
+            //=>让当前旋转的角度成为下一次起始的角度
+            this.rotateX = rotateX;
+            this.rotateY = rotateY;
+        }
+        //=>清空其它记录的自定义属性值
+        ['strX', 'strY', 'changeX', 'changeY'].forEach(item => this[item] = null);
+    };
+
     return {
-        init:function(){
-            $('.cubeBox').css('display','block');
+        init: function () {
+            $cubeBox.css('display', 'block');
+
+            //=>手指操作CUBE,让CUBE跟着旋转
+            let cube = $cube[0];
+            cube.rotateX = -35;
+            cube.rotateY = 35;//=>记录初始的旋转角度（存储到自定义属性上）
+            $cube.on('touchstart', start)
+                .on('touchmove', move)
+                .on('touchend', end);
+
+            //=>点击每一个面跳转到详情区域对应的页面
+            $cubeList.tap(function () {
+                $cubeBox.css('display', 'none');
+
+                //=>跳转到详情区域,通过传递点击LI的索引,让其定位到具体的SLIDE
+                let index = $(this).index();
+                detailRender.init(index);
+            });
         }
     }
 })();
+
+/*DETAIL*/
+let detailRender = (function () {
+    let $detailBox = $('.detailBox'),   
+        swiper = null,
+        $dl = $('.page1>dl');
+
+    let swiperInit = function swiperInit(){
+        let swiper = new Swiper ('.swiper-container', {
+            effect : 'coverflow',
+            onInit: move,
+            onTransitionEnd: move
+        })
+    }
+
+    let move = function move(swiper) {
+        //=>SWIPER:当前创建的实例
+        //1.判断当前是否为第一个SLIDE:如果是让3D菜单展开,不是收起3D菜单
+        let activeIn = swiper.activeIndex,
+            slideAry = swiper.slides;
+        if (activeIn === 0) {
+            //=>PAGE1
+            $dl.makisu({
+                selector: 'dd',
+                overlap: 0.6,
+                speed: 0.8
+            });
+            $dl.makisu('open');
+        } else {
+            //=>OTHER PAGE
+            $dl.makisu({
+                selector: 'dd',
+                speed: 0
+            });
+            $dl.makisu('close');
+        }
+
+        //2.滑动到哪一个页面，把当前页面设置对应的ID，其余页面移除ID即可
+        slideAry.forEach((item, index) => {
+            if (activeIn === index) {
+                item.id = `page${index + 1}`;
+                return;
+            }
+            item.id = null;
+        });
+    };
+
+    return {
+        init:function(index=0){
+           
+            $detailBox.css('display','block');
+            if (!swiper) {
+                //=>防止重复初始化
+                swiperInit();
+            }
+           
+        }
+    }
+})();
+
+
+
+
 /*HASH*/
 let url = window.location.href, 
     well = url.indexOf('#'),
@@ -257,6 +380,9 @@ switch (hash) {
         break;
     case 'cube':
         cubeRender.init();
+        break;
+    case 'detail':
+        detailRender.init();
         break;
     default:
         loadingRender.init();
